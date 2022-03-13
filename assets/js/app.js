8c26c30a74ca85bbc511790079f40db57e97d8ac
ilantimerman <ilan.tmrmn@gmail.com>
2022-03-13 16:08:33 -0700

    add inquirer prompts

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,34 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const mysql = require('mysql');
+
+function vaidateDChoice(){
+    if (choice === 'department'){
+        return true; 
+    }
+}
+
+function addDRE{
+
+    inquirer.prompt([
+        {
+            type: 'confirm',
+            message: 'Would you like to add a new department?',
+            name: 'dChoice',
+
+        },
+        {
+            type: 'confirm',
+            message: 'Would you like to add a new role?',
+            name: 'rChoice'
+        },
+        {
+            type: 'confirm',
+            message: 'Would you like to add a new employee?',
+            name: 'eChoice'
+        }
+
+    ]);
+
+
+} 
