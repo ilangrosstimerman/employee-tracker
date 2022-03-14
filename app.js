cba12ed5b21821ad77db0499c0b0852e4e77cf17
ilantimerman <ilan.tmrmn@gmail.com>
2022-03-13 17:45:31 -0700

    updating inquirer

--- app.js
+++ app.js
@@ -9,27 +9,53 @@ function vaidateDChoice(){
     }
 }
 
-function drePrompts(){
+function startScreen(){
 
     inquirer.prompt([
         {
-            type: 'confirm',
-            message: 'Would you like to add a new department?',
-            name: 'dChoice',
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'choice',
+            choices: [
+             "View All Employees", 
+              "View All Employee's By Roles",
+              "View all Employees By Departments", 
+              "Update Employee",
+              "Add Employee",
+              "Add Role",
+              "Add Department"
+            ]
 
-        },
-        {
-            type: 'confirm',
-            message: 'Would you like to add a new role?',
-            name: 'rChoice'
-        },
-        {
-            type: 'confirm',
-            message: 'Would you like to add a new employee?',
-            name: 'eChoice'
-        }
-
-    ]);
-
-
-} 
+        ]).then(function(result){
+            switch (result.choice){
+                case "View All Employees":
+                viewAllEmployees();
+                break;
+    
+                case "View All Employees by Roles":
+                viewAllEmployeesByRoles();
+                break;
+    
+                case "View All Employees by Departments":
+                viewAllEmployeesByDepartments();
+                break;
+    
+                case "Update Employee":
+                updateEmployee();
+                break;
+    
+                case "Add Employee":
+                addEmployee();
+                break;
+    
+                case "Add Role":
+                addRole();
+                break;
+    
+                case "Add Department":
+                addDepartment();
+                break;
+            }
+        })
+    
+    };
