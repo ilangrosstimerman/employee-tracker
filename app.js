5929fc70689c0bdd2ec5ac2b0e415814664060f7
ilantimerman <ilan.tmrmn@gmail.com>
2022-03-20 10:10:55 -0700

    update README.md

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 const mysql = require("mysql");
-const chalk = require("chalk")
 const clear = require('clear');
-const figlet = require('figlet');
 function initialLoad() {
   clear();
 }
